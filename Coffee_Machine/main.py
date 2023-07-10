f728d5971a711658edfcc2b00fa4eed043a72948
ethomps33 <e.thomps33@gmail.com>
2023-07-09 17:23:35 -0700

    Coffee Machine project

---
+++ Coffee_Machine/main.py
@@ -0,0 +1,58 @@
+# This is a sample Python script.
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+
+# TODO: 1 Print all of the coffee machine resources
+
+for resource in resources:
+    print(f"{resource}: {resources[resource]}ml")
+
+#TODO: 2 Print the prices of all the coffee available
+
+for item in MENU:
+    print(f"{item} price: ${MENU[item]['cost']}")
+
+#TODO: 3 Ask user what drink they want to order
+#Create an input statement to retreive user order
+
+order = input("What is your order? 'Espresso'/'Latte'/'Cappuccino': ").lower()
+
+#TODO: 4 Create If Statements to check if there are available resources for the order
+
+if order == 'espresso':
+    for ingr in MENU[order]:
+        ingrs = [MENU[order][ingr]]
+        print(ingrs)
+
+
