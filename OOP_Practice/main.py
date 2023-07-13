22fa09a9c5420c2027e0d75e51e31288ea52a051
ethomps33 <e.thomps33@gmail.com>
2023-07-12 20:44:47 -0700

    Added screen, turtle and turtle color to it

--- OOP_Practice/main.py
+++ OOP_Practice/main.py
@@ -1,7 +1,10 @@
 from turtle import Turtle, Screen
 
 timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("DarkGreen","aquamarine3")
 print(timmy)
 
-
-
+my_screen = Screen()
+print(my_screen.canvheight,my_screen.canvwidth)
+my_screen.exitonclick()
