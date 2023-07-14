e3c68e23bb465d38e3cb633216700fd37e40d10a
ethomps33 <e.thomps33@gmail.com>
2023-07-13 19:50:46 -0700

    Created table and aligned it "left"

--- OOP_Practice/main.py
+++ OOP_Practice/main.py
@@ -1,12 +1,20 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+#
+#
+# timmy = Turtle()
+# timmy.shape("turtle")
+# timmy.color("aquamarine3", "DarkGreen")
+# timmy.fd(100)
+# print(timmy)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight,my_screen.canvwidth)
+# my_screen.exitonclick()
+from prettytable import PrettyTable
 
+table = PrettyTable()
+table.add_column("Pokemon Name",["Pikachu","Squirtle","Charmander"])
+table.add_column("Type",["Electric","Water","Fire"])
+table.align = "l"
 
-timmy = Turtle()
-timmy.shape("turtle")
-timmy.color("aquamarine3", "DarkGreen")
-timmy.fd(100)
-print(timmy)
-
-my_screen = Screen()
-print(my_screen.canvheight,my_screen.canvwidth)
-my_screen.exitonclick()
+print(table)
