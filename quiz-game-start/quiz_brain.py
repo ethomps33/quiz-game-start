ca7b51de02de391f5d4ef18f07ca989e86f3478a
ethomps33 <e.thomps33@gmail.com>
2023-07-16 21:54:41 -0700

    simplified still_has_ques method

--- quiz-game-start/quiz_brain.py
+++ quiz-game-start/quiz_brain.py
@@ -1,28 +1,3 @@
-# from data import question_data
-# import question_base
-# question = question_base.Question()
-# score = 0
-#
-#
-# class Checker:
-#
-#     def __init__(self, answer,turns):
-#         self.answer = answer
-#         self.score = score
-#         self.turns = turns
-#
-#     def brain(self):
-#         if self.answer == question.quiz():
-#             self.score +=1
-#             print("You got it right!\n"
-#                   f"The answer was: {self.answer}\n"
-#                   f"Your score is now: {self.score}/{self.turns}")
-#             return self.score
-#         else:
-#             print("That's Wrong.\n"
-#                   f"The correct answer was: {self.answer}\n"
-#                   f"Your score is now: {self.score}/{self.turns}")
-#             return self.score
 class QuizBrain:
 
     def __init__(self, list):
@@ -30,11 +5,7 @@ class QuizBrain:
         self.ques_list = list
 
     def still_has_ques(self):
-        if self.ques_number < len(self.ques_list):
-            return True
-        else:
-            return False
-
+        return self.ques_number < len(self.ques_list)
 
     def next_question(self):
         current_ques = self.ques_list[self.ques_number]
